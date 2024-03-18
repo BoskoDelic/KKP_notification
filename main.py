77ae016bd8d92580026347940d9c5c4812aa12c5
bosko <boskod1709@gmail.com>
2024-03-18 17:23:17 +0100

    Set image and window size

--- main.py
+++ main.py
@@ -4,12 +4,19 @@ from bs4 import BeautifulSoup
 from kivy.app import App
 from kivy.uix.boxlayout import BoxLayout
 from kivy.uix.image import AsyncImage
+from kivy.uix.label import Label
+from kivy.config import Config
+
+Config.set("graphics", "width", "500")
+Config.set("graphics", "height", "200")
+Config.set("graphics", "resizable", "0")
 
 class WebScraper:
     def __init__(self, url):
         self.url = url
         self.home_link = None
         self.away_link = None
+        self.date = None
 
     def fetch_div_img(self, div):
         image = div.find("img")
@@ -26,13 +33,13 @@ class WebScraper:
             sys.exit(1)
 
         soup = BeautifulSoup(response.text, "html.parser")
-        next_match_div = soup.find("div", class_="next-match-wrapper")
+        next_match_div = soup.find("div", class_ = "next-match-wrapper")
         if not next_match_div:
             print("Failed to fetch next match div")
             sys.exit(1)
 
-        home_div = next_match_div.find("div", class_="team-1")
-        away_div = next_match_div.find("div", class_="team-2")
+        home_div = next_match_div.find("div", class_ = "team-1")
+        away_div = next_match_div.find("div", class_ = "team-2")
 
         if not home_div:
             print("Failed to fetch home team div")
@@ -43,7 +50,7 @@ class WebScraper:
 
         self.home_link = self.fetch_div_img(home_div)
         self.away_link = self.fetch_div_img(away_div)
-        
+
 
 class ImageFetcherApp(App):
     def __init__(self, url, **kwargs):
@@ -51,13 +58,14 @@ class ImageFetcherApp(App):
         self.scraper = WebScraper(url)
 
     def build(self):
+        self.title = "KKP Notification"
         self.scraper.scrape()
-        root = BoxLayout(orientation='horizontal', spacing=100)
+        root = BoxLayout(orientation = 'horizontal', spacing = 50)
 
-        self.image1 = AsyncImage(source=self.scraper.home_link)
+        self.image1 = AsyncImage(source = self.scraper.home_link, size_hint = (None, None), size = (200, 200))
         root.add_widget(self.image1)
 
-        self.image2 = AsyncImage(source=self.scraper.away_link)
+        self.image2 = AsyncImage(source = self.scraper.away_link, size_hint = (None, None), size = (200, 200))
         root.add_widget(self.image2)
 
         return root
